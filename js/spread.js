75799f1216d9c38515cb5387cccc4b36ebdfe987
icodingforfood <iprogrammer@live.cn>
2015-10-10 17:15:12 +0800

    might be a bug for mobile device

--- js/spread.js
+++ js/spread.js
@@ -225,11 +225,34 @@ function onClick(event) {
 
 }
 
+
+
 function start() {
-	var event = {
+	var isPC = false;
+	isPC = function () {
+		var sUserAgent = navigator.userAgent.toLowerCase();
+		var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+		var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+		var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+		var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+		var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+		var bIsAndroid = sUserAgent.match(/android/i) == "android";
+		var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+		var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+		if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	var event = isPC ? {
 		pageX: window.innerWidth / 2,
 		pageY: window.innerHeight / 2
-	}
+	} : {
+			pageX: window.innerWidth / 2,
+			pageY: window.innerHeight / 4
+		}
 
 	onClick(event);
 }
