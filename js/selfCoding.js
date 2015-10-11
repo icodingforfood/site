59d886af1041337e0f7acbc851268774ad143f75
icodingforfood <iprogrammer@live.cn>
2015-10-11 17:47:40 +0800

    resume

--- js/selfCoding.js
+++ js/selfCoding.js
@@ -14,6 +14,6 @@ function writeStyles(message, index, interval){
 }
 
 (function(){
-	var styleText = " /**\n*hi...\n*\n*你好！... こんにちは...안녕 하세요.bonjour...\n*我是虞浩，劳拉客户端程序员...\n*现在正在学习Javascript & CSS & HTML5\n*/\n#style-text {\n  width: 400px; \n  color: white;\n  background-color: black;\n  padding: 24px;\n  margin: 24px auto;\n}";
+	var styleText = " /**\n*\n*hi...\n*\n*你好！... こんにちは...안녕 하세요.bonjour...\n*我是虞浩，劳拉客户端程序员\n*（欢迎访问http://www.larahits.com）...\n*现在正在学习Javascript & CSS & HTML5\n*/\n*\n#style-text {\n  width: 400px; \n  color: white;\n  background-color: black;\n  padding: 24px;\n  margin: 24px auto;\n}";
 	writeStyles(styleText, 0, 16);
 }).call(this);
