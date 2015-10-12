b1b1faef71c3dacd2f5264d5f3d102f80218b3de
icodingforfood <iprogrammer@live.cn>
2015-10-12 18:32:25 +0800

    resume

--- js/selfCoding.js
+++ js/selfCoding.js
@@ -1,19 +1,25 @@
-function writeStyleChar(char){
+function writeStyleChar(char, appendChar) {
 	var styles = $('#style-text').html() + char;
 	$('#style-text').html(styles);
-    $('#style-tag').append(char);	
+	if (appendChar) {
+		$('#style-tag').append(char);
+	}
 }
 
-function writeStyles(message, index, interval){
-	if(index < message.length){
-		writeStyleChar(message[index++]);
-		return setTimeout(function() {
-			return writeStyles(message,index,interval);
+function writeStyles(message, index, interval, appendChar) {
+	var pre;
+	if (index < message.length) {
+		pre = document.getElementById('style-text');
+        pre.scrollTop = pre.scrollHeight;
+		writeStyleChar(message[index++], appendChar);
+		return setTimeout(function () {
+			return writeStyles(message, index, interval, appendChar);
 		}, interval);
 	}
 }
 
-(function(){
-	var styleText = " /**\n*\n*hi...\n*\n*你好！... こんにちは...안녕 하세요.bonjour...\n*我是虞浩，劳拉客户端程序员\n*（欢迎访问http://www.larahits.com）...\n*现在正在学习Javascript & CSS & HTML5\n*/\n*\n#style-text {\n  width: 400px; \n  color: white;\n  background-color: black;\n  padding: 24px;\n  margin: 24px auto;\n}";
-	writeStyles(styleText, 0, 16);
+(function () {
+	$.get("../resume.css", function (data) {
+		writeStyles(data, 0, 16, true);
+	});
 }).call(this);
