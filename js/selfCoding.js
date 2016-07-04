8123499f76399f9eb5072b2bee9452dce7360e02
icodingforfood <iprogrammer@live.cn>
2016-07-04 09:03:18 +0800

    - add product show page

--- js/selfCoding.js
+++ js/selfCoding.js
@@ -9,13 +9,29 @@ function writeStyleChar(char, appendChar) {
 function writeStyles(message, index, interval, appendChar) {
 	var pre;
 	if (index < message.length) {
-		pre = document.getElementById('style-text');
+		pre = $('#style-text');
         pre.scrollTop = pre.scrollHeight;
 		writeStyleChar(message[index++], appendChar);
 		return setTimeout(function () {
 			return writeStyles(message, index, interval, appendChar);
 		}, interval);
 	}
+}
+
+function loading() {
+	setTimeout(function () {
+		var loopCount = 0;
+		var loadingText = ['-', '\\', '|', '/'];
+		var loadingEffect = setInterval(function () {
+			var idx = loopCount % 4;
+			$('#loading').html(loadingText[idx]);
+			loopCount++;
+			if (loopCount > 200) {
+				clearInterval(loadingEffect);
+				$('#loading').hide();
+			}
+		}, 100);
+	}, 1000);
 }
 
 (function () {
